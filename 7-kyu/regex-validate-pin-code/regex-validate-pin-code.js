d1cf521b00859abd82c7ae2d66952edb85d06026
Fadma1234 <fadma.belkhouraf@gmail.com>
2025-06-28 12:09:13 -0400

    Update solution - CodeHub

--- 7-kyu/regex-validate-pin-code/regex-validate-pin-code.js
+++ 7-kyu/regex-validate-pin-code/regex-validate-pin-code.js
@@ -1,10 +1,2 @@
-function validatePIN (pin) {
-  if(pin.length != 4 && pin.length != 6  ){
-    return false
-  }
-  for ( let i =0; i < pin.length; i++){
-    if( pin[i] < '0' || pin[i] > '9')
-      return false
-  }
-  return true
-}
+const validatePIN = pin => /^(\d{4}|\d{6})$/.test(pin)
+  
